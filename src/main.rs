94354d82e221ad6878ddd1eac5c217040a2e5862
rrakea <login@konradburgi.com>
2025-05-25 14:20:20 +0200

    Started on eval function, refactor done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-mod engine;
-mod util;
+pub mod cli;
+pub mod eval;
+pub mod move_gen;
+pub mod tree_search;
 
 const FEN: &str = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1.";
 
 fn main() {
-    let start = util::fen::fen_to_board(util::fen::START);
-    util::draw::draw(&start, 0.0, 0.0, 8);
+    let start = move_gen::fen::fen_to_board(move_gen::fen::START);
+    cli::draw::draw(&start, 0.0, 0.0, 8);
     /*
     for m in moves {
         println!("{}", util::draw::prittify_move(&start, m));
@@ -18,14 +20,14 @@ fn main() {
     }
 }
 
-fn num_positions(p: &util::state::State, depth: u8) -> u64 {
+fn num_positions(p: &move_gen::state::State, depth: u8) -> u64 {
     if depth == 0 {
         return 1;
     }
-    let moves = util::moves::moves(p);
+    let moves = move_gen::move_gen::moves(p);
     let mut tally = 0;
     for m in moves {
-        let o = util::outcome::outcome(p, m);
+        let o = move_gen::outcome::outcome(p, m);
         tally += num_positions(&o, depth - 1);
     }
     tally
