fc7e1044ef5ec8134db725ae23ac2014aeb87b9c
rrakea <login@konradburgi.com>
2025-08-27 13:14:19 +0200

    .

--- rosa-lib/src/mv.rs
+++ rosa-lib/src/mv.rs
@@ -1,4 +1,5 @@
 use crate::pos::Pos;
+use crate::util;
 
 /*
     Move encoding as u32
@@ -200,6 +201,7 @@ impl Mv {
     }
 
     pub fn notation(&self) -> String {
-        String::new()
+        let (start, end) = self.sq();
+        let util::file
     }
 }
