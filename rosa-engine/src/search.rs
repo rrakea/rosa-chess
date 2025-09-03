a14e795c9d9164bcb4d11f609f81e678d204fd01
rrakea <login@konradburgi.com>
2025-09-03 15:50:25 +0200

    .

--- rosa-engine/src/search.rs
+++ rosa-engine/src/search.rs
@@ -274,7 +274,7 @@ pub fn counting_search(p: &mut pos::Pos, depth: u8) -> u64 {
 pub fn division_search(p: &mut pos::Pos, depth: u8) {
     let mut total = 0;
     TT.resize(10000);
-    for mut mv in mv::mv_gen::gen_mvs(p){
+    for mut mv in mv::mv_gen::gen_mvs(p) {
         make(p, &mut mv, true);
         let count = counting_search(p, depth - 1);
         total += count;
