2c5cab4eb539154e6f68dd8fba42e39878ddb7d8
rrakea <login@konradburgi.com>
2025-10-20 15:28:25 +0200

    Sprt script

--- sprt.sh
+++ sprt.sh
@@ -2,7 +2,15 @@
 
 git checkout dev
 cargo build --release
---target-dir
+cp ./target/release/rosa-engine ./sprt_builds/main
+git checkout main
+cargo build --release
+cp ./target/release/rosa-engine ./sprt_builds/dev
 
+# Opening books from: https://github.com/official-stockfish/books
+ 
 fastchess \
--engine cmd=rosa
+-engine cmd=./sprt_builds/main \
+-engine cmd=./sprt_builds/dev \
+-openings file=./sprt_builds/8moves_v3.pgn format=pgn order=random \
+-sprt elo0=0 elo1=2 alpha=0.05 beta=0.05
