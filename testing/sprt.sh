c958a2b30a8d9921e60463b2c39bcb12f911ecc3
rrakea <login@konradburgi.com>
2026-01-21 21:32:26 +0100

    changed sprt time

--- testing/sprt.sh
+++ testing/sprt.sh
@@ -5,7 +5,7 @@ path="/home/rrakea/code/rosa"
 fastchess -engine cmd="${path}/testing/baseline" name=Baseline\
     -engine cmd="${path}/target/release/rosa-engine" name=Test\
     -openings file=8moves_v3.pgn format=pgn order=random\
-    -each tc=15+1\
+    -each tc=10+0.1\
     -rounds 50\
     -concurrency 4\
     -log file=${path}/testing/log.txt level=warn engine=true\
