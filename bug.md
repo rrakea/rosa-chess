090153e60a4420a91c87cdda81da37ec1e55a02f
rrakea <login@konradburgi.com>
2026-02-22 21:24:24 +0100

    added bug.md

---
+++ bug.md
@@ -0,0 +1,7 @@
+# Repetition Bug Thoughts
+
+Rosa goes for a repetition draw in completely winning positions
+Option:
+
+- Repetition detection does not work (e.g. it only thinks after 4 positions is a repetition)
+- Repetitions dont get propagated upwards correctly
--- testing/sprt.sh
+++ testing/sprt.sh
@@ -6,9 +6,9 @@ fastchess\
     -engine cmd="${path}/target/release/rosa-engine" name=Test\
     -engine cmd="${path}/testing/baseline" name=Baseline\
     -openings file=8moves_v3.pgn format=pgn order=random\
-    -each tc=10+0.1\
-    -rounds 50\
-    -concurrency 8\
+    -each tc=2+0.1\
+    -rounds 100\
+    -concurrency 4\
     -log file=${path}/testing/log.txt level=warn\
 
 echo -e "\n\n\n\n\n" >> ${path}/testing/log.txt
